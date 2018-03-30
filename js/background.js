bd2c8bf5fdca55b55810fdea9c9ef3d495b6b552
Castro <dvasuta@gmail.com>
2018-03-31 00:48:55 +0300

    Init

---
+++ js/background.js
@@ -0,0 +1,63 @@
+// Fired when the extension is first installed, when the extension is updated to a new version, and when Chrome is updated to a new version
+// http://developer.chrome.com/extensions/runtime.html#event-onInstalled
+chrome.runtime.onInstalled.addListener(function (details) {
+
+    // good place to set default options
+    function setDefaults(callback) {
+        storage.area.get(function (stored_options) {
+            var default_options = storage.default_options,
+                option,
+                new_options = {};
+            for (option in default_options) {
+                if (!stored_options.hasOwnProperty(option)) {
+                    new_options[option] = default_options[option];
+                }
+            } 
+            if (Object.keys(new_options).length !== 0) {
+                // save to area if new default options is appeared
+                storage.area.set(new_options, function () {
+                    if (typeof callback === 'function') {
+                        callback();
+                    }
+                });
+            } else {
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            }
+        });
+    }
+
+    switch (details.reason) {
+    case 'update':
+        setDefaults();
+        break;
+    default:
+        break;
+    }
+});
+
+chrome.runtime.onUpdateAvailable.addListener(function (details) {
+    // when an update is available - reload extension
+    // update will be install immediately
+    chrome.runtime.reload();
+});
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.query(
+        { currentWindow: true, active: true },
+        function (tabArray) {
+
+   if (tabArray.length !== 0 && tabArray[0].status == 'complete') {   
+debugger;
+            chrome.tabs.executeScript(tabArray[0].id, {
+                file: 'js/content_script.js'
+             }, function() { 
+
+            })
+        }
+
+        }
+    );
+}); 
---
+++ js/content_script.js
@@ -0,0 +1,73 @@
+
+	if($('#hepart_button').length === 0) {
+	 	drawHepardButton();
+     }
+
+ 
+
+
+
+function drawHepardButton(){
+	//setTimeout(function () {
+		
+			var d = document.createElement('span');
+			$(d).attr('id', 'hepart_button')
+				.addClass('active')
+			/*	.css('background', 'url(' + chrome.extension.getURL('img/ext_icons/ic_bus_articulated_front_black_18dp.png') + ') orange no-repeat') */
+			    .html(chrome.i18n.getMessage("hepart_run"))
+			    .prependTo($("#exportLotDetails")) 
+			.click(function () {
+			    $(this).removeClass('active');
+			    $(this).off('click');
+			    getLotinfoById(insertTableRows);
+			});
+		
+
+	// }, 2000);
+}
+
+function getLotinfoById(callback) {
+  var url = window.location.href.replace(/\/$/, ''); 
+  var lotId = url.substr(url.lastIndexOf('/') + 1);
+
+  if (lotId) {
+    $.get("https://www.copart.com/public/data/lotdetails/solr/" + lotId, function(data) {
+      if (data && data.data.lotDetails) {
+        callback && callback(data.data.lotDetails);
+      }
+    }, "json");
+  } else {
+    console.debug('Wrong lot id', lotId);
+  }
+}
+
+function insertTableRows(data) {
+	var sellerRow = document.querySelectorAll('[data-uname~="lotdetailSeller"]');
+	if (sellerRow.length === 0 && data.std && data.snm) {
+		var container = $(document.querySelectorAll('[data-uname~="lotdetailPrimarydamage"]'));
+		container = container.parent().parent();
+		var tmpl = "<div><div class='details hepart_row'><label>" + chrome.i18n.getMessage("hepart_seller_type") + "</label><span class='lot-details-desc col-md-6'>"+ data.std +"</span></div></div>"
+		tmpl += "<div><div class='details hepart_row'><label>" + chrome.i18n.getMessage("hepart_seller_name") + "</label><span  class='lot-details-desc col-md-6'>"+ data.snm +"</span></div></div>"
+		container.prepend($(tmpl));
+	}
+	if (data.rc) {
+		var container = $(document.querySelectorAll('[data-uname~="lotdetailVin"]'));
+		container = container.parent().parent();
+		var tmpl = "<div><div class='details hepart_row'><label>" + chrome.i18n.getMessage("hepart_repair_cost") + "</label><span class='lot-details-desc col-md-6'>"+ formatter.format(data.rc) +" "+ data.cuc+"</span></div></div>"
+		container.prepend($(tmpl));
+	}
+	if (data.ahb !== 0) {
+		var container = $(document.querySelectorAll('[name=counterBidForm] .sold-bid .sold'));
+		var tmpl = "<div class='sold hepart_final_price'>" + chrome.i18n.getMessage("hepart_final_price") + formatter.format(data.ahb) + " " + data.cuc + "</div>"
+		container.after($(tmpl));
+	}
+}
+
+var formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0
+});
+
+
